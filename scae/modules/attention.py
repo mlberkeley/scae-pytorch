8ea73b879fd61af7a7ebd1ff820dfeb0cf9c8ce8
Kenny Workman <kennyworkman@sbcglobal.net>
2021-01-16 16:33:16 -0800

    working set transformer

--- scae/modules/attention.py
+++ scae/modules/attention.py
@@ -1,211 +1,107 @@
-import numpy as np
+"""
+attention.py
+~~~~
+Code taken and modified from official implementation of SetTransformer:
+https://github.com/juho-lee/set_transformer
+"""
+
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import math
 
 
 class SetTransformer(nn.Module):
 
-    """Permutation invariant Transformer."""
-
     def __init__(self,
-                 n_layers,
-                 n_heads,
-                 n_dims,
-                 n_output_dims,
-                 n_outputs,
+                 n_input_dims,  # Last dimension of input
+                 n_layers=5,  # number of attention blocks
+                 n_heads=4,
+                 n_dims=128,  # hidden layer dimension
+                 n_output_dims=32,
+                 n_outputs=3,
                  layer_norm=False,
-                 dropout_rate=0.,
-                 n_inducing_points=0):
+                 n_inducing_points=32):
+
         super(SetTransformer, self).__init__()
-        self._n_layers = n_layers
-        self._n_heads = n_heads
-        self._n_dims = n_dims
-        self._n_output_dims = n_output_dims
-        self._n_outputs = n_outputs
-        self._layer_norm = layer_norm
-        self._dropout_rate = dropout_rate
-        self._n_inducing_points = n_inducing_points
 
-    def forward(self, x, presence=None):
+        enc_modules = [ISAB(n_dims, n_dims, n_heads, n_inducing_points,
+                            ln=layer_norm) for i in range(n_layers-1)]
+        self.enc = nn.Sequential(ISAB(n_input_dims, n_dims, n_heads,
+                                      n_inducing_points, ln=layer_norm),
+                                 *enc_modules)
 
-        batch_size = int(x.shape[0])
+        dec_modules = [SAB(n_dims, n_dims, n_heads, ln=layer_norm)
+                       for i in range(n_layers)]
+        self.dec = nn.Sequential(
+            PMA(n_dims, n_heads, n_outputs, ln=layer_norm),
+            SAB(n_dims, n_dims, n_heads, ln=layer_norm),
+            *dec_modules,
+            nn.Linear(n_dims, n_output_dims))
 
-        # generic linear mlp applied over whole batch
-        h = nn.Linear(self._n_dim)(x)
-
-        args = [self._n_heads, self._layer_norm, self._dropout_rate]
-        klass = SelfAttention
-
-        # TODO: lets avoid induced attention for now on the encoder
-        # if self._n_inducing_points > 0:
-        #     args = [self._n_inducing_points] + args
-        #     klass = InducedSelfAttention
-
-        # From Paper: Encoder(X) := SAB(SAB(X)) : SAB = Self-Attention Block
-        for _ in range(self._n_layers):
-            h = klass(*args)(h, presence)
-        z = nn.Linear(self._n_output_dims)(h)
-
-        # From Paper: Decoder(Z) = rFF(SAB(PMA_k(Z)))
-        # s.t. rFF is a row-wise (sample independent) Feed-Forward Layer
-        # and PMA_k is Pooling by Multihead Attention induced on k seed vectors
-
-        # TODO: avoid induced attention for now.
-        # Unclear where tf global variable is coming from and what we want for
-        # these values.
-
-        # inducing_points = tf.get_variable(
-        #     'inducing_points', shape=[1, self._n_outputs, self._n_output_dims])
-        # inducing_points = snt.TileByDim([0], [batch_size])(inducing_points)
-
-        return MultiHeadQKVAttention(self._n_heads)(z, z, z, presence)
-        # return MultiHeadQKVAttention(self._n_heads)(inducing_points, z, z, presence)
+    def forward(self, X):
+        return self.dec(self.enc(X))
 
 
-class QKVAttention(nn.Module):
+class MAB(nn.Module):
+    def __init__(self, dim_Q, dim_K, dim_V, num_heads, ln=False):
+        super(MAB, self).__init__()
+        self.dim_V = dim_V
+        self.num_heads = num_heads
+        self.fc_q = nn.Linear(dim_Q, dim_V)
+        self.fc_k = nn.Linear(dim_K, dim_V)
+        self.fc_v = nn.Linear(dim_K, dim_V)
+        if ln:
+            self.ln0 = nn.LayerNorm(dim_V)
+            self.ln1 = nn.LayerNorm(dim_V)
+        self.fc_o = nn.Linear(dim_V, dim_V)
 
-    """Generic QKV Attention module.
+    def forward(self, Q, K):
+        Q = self.fc_q(Q)
+        print(K.shape)
+        K, V = self.fc_k(K), self.fc_v(K)
 
-    Att(Q, K, V; w) := w(QK^T)V
-    """
+        dim_split = self.dim_V // self.num_heads
+        Q_ = torch.cat(Q.split(dim_split, 2), 0)
+        K_ = torch.cat(K.split(dim_split, 2), 0)
+        V_ = torch.cat(V.split(dim_split, 2), 0)
 
-    def forward(self, queries, keys, values, presence=None):
-        """
-        :param queries: Tensor of shape [B, N, d_k].
-        :param keys: Tensor of shape [B, M, d_k].
-        :param values: Tensor of shape [B, M, d_v].
-        :param presence: None or tensor of shape [B, M].
-
-        :returns: Tensor of shape [B, N, d_v]
-        """
-
-        n_dim = int(queries.shape[-1])
-
-        # [B, M, d] x [B, d, N] = [B, M, N]
-        routing = torch.matmul(queries, keys.t())
-
-        if presence is not None:
-            presence = torch.unsqueeze(presence, -2).float()
-            routing -= (1. - presence) * 1e32
-
-        routing = nn.Softmax(routing / np.sqrt(n_dim), -1)
-
-        # every output is a linear combination of all inputs
-        # [B, M, N] x [B, N, d_v] = [B, M, d_v]
-        res = torch.matmul(routing, values)
-        return res
+        A = torch.softmax(Q_.bmm(K_.transpose(1, 2))/math.sqrt(self.dim_V), 2)
+        O = torch.cat((Q_ + A.bmm(V_)).split(Q.size(0), 0), 2)
+        O = O if getattr(self, 'ln0', None) is None else self.ln0(O)
+        O = O + F.relu(self.fc_o(O))
+        O = O if getattr(self, 'ln1', None) is None else self.ln1(O)
+        return O
 
 
-class MultiHeadQKVAttention(nn.Module):
+class SAB(nn.Module):
+    def __init__(self, dim_in, dim_out, num_heads, ln=False):
+        super(SAB, self).__init__()
+        self.mab = MAB(dim_in, dim_in, dim_out, num_heads, ln=ln)
 
-    """Adapted multi-head version of `QKVAttention` module.
-
-    Multihead(Q, K, V; w) := concat(O_1, ..., O_h)W^0
-
-    s.t. O_j = w(QK^T)V (Attention on reduced dimension of input)
-    """
-
-    def __init__(self, n_heads):
-        super(MultiHeadQKVAttention, self).__init__()
-        self._n_heads = n_heads
-
-    def forward(self, queries, keys, values, presence=None):
-
-        def transform(x, n=self._n_heads):
-            n_dim = np.ceil(float(int(x.shape[-1])) / n)
-            return nn.Linear(int(n_dim))(x)
-
-        outputs = []
-        for _ in range(self._n_heads):
-            args = [transform(i) for i in [queries, keys, values]]
-            if presence is not None:
-                args.append(presence)
-            outputs.append(QKVAttention()(*args))
-
-        linear = nn.Linear(values.shape[-1])
-        return linear(torch.cat((outputs), dim=-1))
+    def forward(self, X):
+        return self.mab(X, X)
 
 
-class SelfAttention(nn.Module):
+class ISAB(nn.Module):
+    def __init__(self, dim_in, dim_out, num_heads, num_inds, ln=False):
+        super(ISAB, self).__init__()
+        self.I = nn.Parameter(torch.Tensor(1, num_inds, dim_out))
+        nn.init.xavier_uniform_(self.I)
+        self.mab0 = MAB(dim_out, dim_in, dim_out, num_heads, ln=ln)
+        self.mab1 = MAB(dim_in, dim_out, dim_out, num_heads, ln=ln)
 
-    """SAB(X) := MAB(X, X)
-
-    MAB(X, Y) := LayerNorm(H + rFF(H))
-        : H = LayerNorm(X + Multihead(X, Y, Y; w)
-
-    ie. Self-Attention Block is MultiheadAttention where X = Y. """
-
-    def __init__(self, n_heads, layer_norm=False, dropout_rate=0.):
-        super(SelfAttention, self).__init__()
-        self._n_heads = n_heads
-        self._layer_norm = layer_norm
-        self._dropout_rate = dropout_rate
-
-    def forward(self, x, presence=None):
-        n_dims = int(x.shape[-1])
-
-        y = self._self_attention(x, presence)
-
-        if self._dropout_rate > 0.:
-            x = nn.Dropout(self._dropout_rate)(x)
-
-        y += x
-
-        if presence is not None:
-            y *= torch.unsqueeze(presence.float(), -1)
-
-        if self._layer_norm:
-            y = nn.LayerNorm(y[1:])(y)
-
-        # Two layers of row-wise (independent) feed-forward
-        h = nn.Linear(n_dims)(nn.Linear(2*n_dims)(x))
-
-        if self._dropout_rate > 0.:
-            h = nn.Dropout(self._dropout_rate)(h)
-
-        h += y
-
-        if self._layer_norm:
-            h = nn.LayerNorm(h[1:])(h)
-
-        return h
-
-    def _self_attention(self, x, presence):
-        heads = MultiHeadQKVAttention(self._n_heads)
-        return heads(x, x, x, presence)
+    def forward(self, X):
+        H = self.mab0(self.I.repeat(X.size(0), 1, 1), X)
+        return self.mab1(X, H)
 
 
-class InducedSelfAttention(SelfAttention):
+class PMA(nn.Module):
+    def __init__(self, dim, num_heads, num_seeds, ln=False):
+        super(PMA, self).__init__()
+        self.S = nn.Parameter(torch.Tensor(1, num_seeds, dim))
+        nn.init.xavier_uniform_(self.S)
+        self.mab = MAB(dim, dim, dim, num_heads, ln=ln)
 
-    """Self-attention with inducing points a.k.a. ISAB from SetTransformer.
-
-    ISAB(X) := MAB(X, H) : H := MAB(I, X)
-    Induced self-attention on kernel of reduced dimensionality for performance.
-
-    TODO: IMPLEMENT GLOBAL VARIABLE, avoid induced attention for now (can get
-    away with this for constellation/mnist
-    """
-
-    # def __init__(self, n_inducing_points, n_heads, layer_norm=False,
-    #              dropout_rate=0.):
-    #     super(InducedSelfAttention, self).__init__(n_heads, layer_norm,
-    #                                                dropout_rate)
-
-    #     self._n_inducing_points = n_inducing_points
-
-    # def _self_attention(self, x, presence):
-
-    #     head_before = MultiHeadQKVAttention(self._n_heads)
-    #     # head_after = MultiHeadQKVAttention(self._n_heads)
-    #     head_after = head_before
-
-    #     inducing_points = tf.get_variable(
-    #         'inducing_points', shape=[1, self._n_inducing_points,
-    #                                   int(x.shape[-1])])
-
-    #     inducing_points = snt.TileByDim(
-    #         [0], [int(x.shape[0])])(inducing_points)
-
-    #     z = head_before(inducing_points, x, x)
-    #     y = head_after(x, z, z)
-    #     return y
+    def forward(self, X):
+        return self.mab(self.S.repeat(X.size(0), 1, 1), X)
