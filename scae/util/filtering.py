80f592c227b907b7aa85e9cb756541984223c986
Nitish Dashora <nrdashora@gmail.com>
2021-02-10 00:30:00 -0500

    Updated filtering code

--- scae/util/filtering.py
+++ scae/util/filtering.py
@@ -2,10 +2,24 @@ from scipy import ndimage
 import numpy as np
 import torch
 import kornia
+from PIL import Image
 
 def torch_sobel_filter(tensor):
-    out = kornia.filters.spatial_gradient(tensor.unsqueeze(0))
-    return torch.sum(out, dim=2).squeeze(0)
+    out = torch.abs(kornia.filters.spatial_gradient(tensor.unsqueeze(0)))
+    out = torch.sum(out, dim=2)
+    m = torch.max(out)
+    out = torch.div(out, m + 1e-8)
+    #im = Image.fromarray(torch.sum(out, dim=2).detach().cpu().numpy()[0].transpose(2,1,0), mode="RGB")
+    #im.save(r"C:\Users\nrdas\Downloads\MLAB\temp.png")
+    #print("Min, Max:", torch.min(out), torch.max(out))
+
+    #raise ZeroDivisionError
+
+    # print(torch.sum(out, dim=2).detach().cpu().numpy()[0].transpose(1,2,0).shape)
+    # im = Image.fromarray(torch.sum(out, dim=2).detach().cpu().numpy()[0].transpose(2,1,0), mode="RGB")
+    # im.save(r"C:\Users\nrdas\Downloads\MLAB\temp.png")
+    # raise ZeroDivisionError
+    return out.squeeze(0)
 
 def sobel_filter(img):
     return ndimage.sobel(img)
